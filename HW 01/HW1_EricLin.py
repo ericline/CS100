18d8db47f40acd300d9264e5c035d4b6c8a9a38c
Eric <115109937+ericline@users.noreply.github.com>
2022-11-05 15:50:52 -0400

    Add files via upload

---
+++ HW 01/HW1_EricLin.py
@@ -0,0 +1,23 @@
+"""
+Eric Lin
+CS 100 2022F Section 009
+HW 01, September 15th, 2022
+"""
+
+# Exercise 5b
+
+hours = 24;
+minutes = 60;
+seconds = 60;
+
+# Exercise 5c
+
+pi = 3.14159;
+euler = 2.71812;
+mascheroni = 0.57721;
+
+# Exercise 5d
+
+firstName = 'Eric';
+lastName = 'Lin';
+currentSchool = 'NJIT';
